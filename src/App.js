069d28783fd47988b98af1d8d78ad41797abeb60
jhon11390 <meljhon2@gmail.com>
2020-04-10 01:01:50 -0500

    Se crea filtro para los cursos posts

--- src/App.js
+++ src/App.js
@@ -5,22 +5,40 @@ import posts from './posts'
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      filtergen: '',
+      filterscourses: [],
+    }
+  }
   render() {
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input type="text" placeholder="Ingresa el término de búsqueda" value={this.state.filtergen} onChange={this.updateFilter.bind(this)}/>
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+          {posts.filter(post => post.title.toLowerCase().includes(this.state.filtergen.toLowerCase())).map(filterpost => (
+            <li>
+              <a href={filterpost.url}><img src={filterpost.image } /></a>
+              <p>{filterpost.title}</p>
+            </li>
+          ))}
         </ul>
       </div>
     )
   }
+
+  updateFilter(event){
+    this.setState({
+      filtergen: event.target.value
+    })
+  }
 }
+
+
 
 
 export default App
